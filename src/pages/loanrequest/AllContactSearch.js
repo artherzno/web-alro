4815b31555f352396b7ab97c21df55a7f92858ac
artherzno <artherzno@gmail.com>
2021-05-16 00:43:16 +0700

    - Add AllContactSearch page

--- src/pages/loanrequest/AllContactSearch.js
+++ src/pages/loanrequest/AllContactSearch.js
@@ -1,9 +1,138 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import Fade from '@material-ui/core/Fade';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
+
+import Header from '../../components/Header';
+import Nav from '../../components/Nav';
+import { 
+    MuiTextfield,
+    ButtonNormalIconStartPrimary,
+} from '../../components/MUIinputs';
+
+
+// All Data for DataGrid & Table ---------------------------------------------//
+
+const tableResult = [
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+    { a: 'RIET16310/00002', b: '13/07/2020', c: '163/00071', d: '00551', f: 'เพื่อส่งเสริมการปลูกข้าวหอมมะลิ', g: '00137/2562', h: '10/05/2019', i: '2287654478986', j: 'นาง', k: 'บัวลี', l: 'บางวิเศษ', m: '165 หมู่4 ตำบลหัวช้าง อำเภอจตุรพักตรพิมาน จังหวัดร้อยเอ็ด'},
+]
+
+// End All Data for DataGrid ---------------------------------------------//
 
 function AllContactSearch() {
+    const history = useHistory();
+
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setLoaded(true);
+    }, [])
+
+    const gotoAddLoanRequestContact = () => {
+        history.push('/loanrequest/loanrequestcontact');
+    }
+    
     return (
-        <div>
-            
+        <div className="loanrequestproject-page">
+            <div className="header-nav">
+                <Header bgColor="bg-light-green" status="logged" />
+                <Nav />
+            </div>
+
+            <Fade in={loaded} timeout={800}>
+                <div className="fade">
+                    <Container maxWidth={false}>
+                        <Grid container spacing={1}>
+                            <Grid item xs={12} md={12} className="title-page"> 
+                                <h1>สัญญาทั้งหมด</h1>
+                            </Grid>
+                            <Grid item xs={12} md={12} className="mg-t-20">
+                                <Grid container spacing={1}>
+                                    <Grid item xs={12} md={3}>
+                                        {/* Field Text ---------------------------------------------------*/}
+                                        <MuiTextfield label="ค้นหาชื่อ-นามสกุล" defaultValue="" />
+                                    </Grid>
+                                    <Grid item xs={12} md={3}>
+                                        {/* Field Text ---------------------------------------------------*/}
+                                        <MuiTextfield label="ค้นหาเลขที่สัญญา" defaultValue="" />
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+
+                            <Grid item xs={12} md={12}>
+                                <div className="table-box table-allcontactsearch1">
+                                    <Table aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell align="center">รหัสบันทึก</TableCell>
+                                                <TableCell align="center">วันที่บันทึก</TableCell>
+                                                <TableCell align="center">เลขคำขอ</TableCell>
+                                                <TableCell align="center">รหัสโครงการ</TableCell>
+                                                <TableCell align="center">ชื่อโครงการ</TableCell>
+                                                <TableCell align="center">เลขที่สัญญา</TableCell>
+                                                <TableCell align="center">วันที่กู้</TableCell>
+                                                <TableCell align="center">เลขบัตรประชาชน</TableCell>
+                                                <TableCell align="center">คำนำหน้า</TableCell>
+                                                <TableCell align="center">ชื่อ</TableCell>
+                                                <TableCell align="center">นามสกุล</TableCell>
+                                                <TableCell align="center">ที่อยู่</TableCell>
+                                                <TableCell align="center" className="sticky tb-w-14em">&nbsp;</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {tableResult.map((row,i) => (
+                                                <TableRow key={i}>
+                                                   <TableCell align="center">{row.a}</TableCell>
+                                                        <TableCell align="center">{row.b}</TableCell>
+                                                        <TableCell align="center">{row.c}</TableCell>
+                                                        <TableCell align="center">{row.d}</TableCell>
+                                                        <TableCell align="center">{row.f}</TableCell>
+                                                        <TableCell align="center">{row.g}</TableCell>
+                                                        <TableCell align="center">{row.h}</TableCell>
+                                                        <TableCell align="center">{row.i}</TableCell>
+                                                        <TableCell align="center">{row.j}</TableCell>
+                                                        <TableCell align="center">{row.k}</TableCell>
+                                                        <TableCell align="center">{row.l}</TableCell>
+                                                        <TableCell align="center">{row.m}</TableCell>
+                                                    <TableCell align="center" className="sticky tb-w-24em">
+                                                            <ButtonNormalIconStartPrimary label="แก้ไข" onClick={()=>gotoAddLoanRequestContact()} />
+                                                            <ButtonNormalIconStartPrimary label="พิมพ์การ์ดรายสัญญา" />
+                                                </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </div>
+                                {/* Data Grid --------------------------------*/}
+                                    {/* <div style={{ height: 400, width: '100%' }}>
+                                    <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+                                </div> */}
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </div>
+            </Fade>
+
         </div>
     )
 }
