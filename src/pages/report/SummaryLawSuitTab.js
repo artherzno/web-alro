607f7cb69acb5882144dbe485f92231d3c0cf705
surinsom <surinsom@gmail.com>
2022-05-29 13:26:10 +0700

    Add onChangeYearBudget

--- src/pages/report/SummaryLawSuitTab.js
+++ src/pages/report/SummaryLawSuitTab.js
@@ -283,6 +283,13 @@ parameter.append('YearTo', YearTovalue);
                                         }, () => {
                                         })
                                     }}
+                                    onChangeYearBudget={(event) => {
+                                        this.setState({
+                                            year: event.target.value,
+                                            yearLabel: event.target.value
+                                        }, () => {
+                                        })
+                                    }}
                             />
                         </Grid>
                         
